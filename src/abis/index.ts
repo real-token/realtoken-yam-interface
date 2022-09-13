bd28a2960e0dd51e2d16d076c522cb72fedee096
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2022-09-13 23:24:35 +0200

    fix: move json files to a folder

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,6 +1,6 @@
-export { default as ERC20ABI } from 'src/abis/ERC20.json';
-export { default as bridgeTokenABI } from 'src/abis/bridgeToken.json';
-export { default as swapCatUpgradeableABI } from 'src/abis/SwapCatUpgradeable.json';
+export { default as ERC20ABI } from 'json/ERC20.json';
+export { default as bridgeTokenABI } from 'json/bridgeToken.json';
+export { default as swapCatUpgradeableABI } from 'json/SwapCatUpgradeable.json';
 
 export * from 'src/abis/types';
 export * from 'src/abis/types/SwapCatUpgradeable';
