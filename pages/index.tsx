c66e2a42b5eca50f111e402584fbd80aaf369fd0
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2022-09-13 23:47:26 +0200

    fix: rename lowercase

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import { NextPage } from 'next';
 
 import { MarketSell } from 'src/components/Market';
 import { MarketTableRow } from 'src/components/Market/MarketTableRow';
-import { MarketDashboard } from 'src/modules/market';
 
 const HomePage: NextPage = () => {
   return (
     <Fragment>
       <MarketTableRow />
-      {/* <MarketDashboard /> */}
       <MarketSell />
     </Fragment>
   );
--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,6 +1,6 @@
-export { default as Erc20ABI } from 'json/Erc20.json';
+export { default as Erc20ABI } from 'json/erc20.json';
 export { default as bridgeTokenABI } from 'json/bridgeToken.json';
-export { default as swapCatUpgradeableABI } from 'json/SwapCatUpgradeable.json';
+export { default as swapCatUpgradeableABI } from 'json/swapCatUpgradeable.json';
 
 export * from 'src/abis/types';
 export * from 'src/abis/types/SwapCatUpgradeable';
