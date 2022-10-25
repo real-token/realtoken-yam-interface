2cefb31a3fe6a7f75e87043143a42df785f40dd0
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2022-10-25 14:34:24 +0200

    fix: rechange layout to use mantine component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,16 +1,17 @@
 import { FC, ReactNode } from 'react';
 
+import { AppShell } from '@mantine/core';
+
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
+import { styles } from './Layout.styles';
 
-const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+type LayoutProps = { children: ReactNode };
+
+export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <AppShell header={<Header />} footer={<Footer />} styles={styles.appShell}>
+      {children}
+    </AppShell>
   );
 };
-
-export default Layout;
