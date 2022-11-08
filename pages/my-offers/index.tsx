d5e5129328fd7ee85397c015694fd4f479fd2252
Michael <michael@realt.co>
2022-11-08 15:12:38 +0100

    move the offer creation and redesign of the part your offers

--- pages/my-offers/index.tsx
+++ pages/my-offers/index.tsx
@@ -1,11 +1,38 @@
 import { Fragment } from 'react';
+import { SellActions } from 'src/components/Market';
 
 import { MarketTableUser } from 'src/components/Market/MarketTable';
 
+import { Tabs } from '@mantine/core';
+import { IconList, IconPlus, IconFingerprint } from '@tabler/icons';
+import { useTranslation } from 'react-i18next';
+
 const TransfersPage = () => {
+  const menu = useTranslation('menu', { keyPrefix: 'subMenuMyOffer' });
+  const  common = useTranslation('common', { keyPrefix: 'general' });
   return (
     <Fragment>
-      <MarketTableUser />
+      <Tabs color={"orange"} defaultValue={"myOffers"}>
+        <Tabs.List>
+          <Tabs.Tab value={"myOffers"} icon={<IconList size={18} />}>{menu.t('myOffers')}</Tabs.Tab>
+          <Tabs.Tab value={"addOffer"} icon={<IconPlus size={18} />}>{menu.t('addOffer')}</Tabs.Tab>
+          <Tabs.Tab value={"privateOffers"} icon={<IconFingerprint size={18} />}>{menu.t('privateOffers')}</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value={"myOffers"} pt={"xs"}>
+          <MarketTableUser />
+        </Tabs.Panel>
+
+        <Tabs.Panel value={"addOffer"} pt={"xs"}>
+          <SellActions />
+        </Tabs.Panel>
+
+        <Tabs.Panel value={"privateOffers"} pt={"xs"}>
+          <div>{common.t("comingSoon")}</div>
+        </Tabs.Panel>
+
+      </Tabs>
+      
     </Fragment>
   );
 };
