3f1db476e2508b9260506d78fe8a628ba2651a2e
Jean-Benoît <jeanbenoit@realt.co>
2022-12-06 15:29:28 +0100

    feat: add filter input

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import { NextPage } from 'next';
 
 import { MarketTable } from 'src/components/Market';
 import 'src/components/Market';
+import { MarketTableFilter } from 'src/components/Market/Filters';
 
 const HomePage: NextPage = () => {
   return (
     <Fragment>
+      <MarketTableFilter />
       <MarketTable />
     </Fragment>
   );
--- src/states/config.ts
+++
@@ -1,3 +0,0 @@
-import { atomWithStorage } from 'jotai/utils'
-
-export const isRefreshedAutoAtom = atomWithStorage<boolean>("isRefreshedAuto",false);
