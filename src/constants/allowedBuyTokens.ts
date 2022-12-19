4547332c57ceeb3e351b1a845afffa240ecbf471
Michael-RealT <Michael@realt.co>
2022-12-19 23:00:54 +0100

    add stableCoin for Ethereum and GnosisChain

--- src/constants/allowedBuyTokens.ts
+++ src/constants/allowedBuyTokens.ts
@@ -1,19 +1,63 @@
-import { AllowedBuyToken } from "src/types/allowedBuyTokens";
+import { AllowedBuyToken } from 'src/types/allowedBuyTokens';
 
 export const goerliAllowedBuyTokens: AllowedBuyToken[] = [
-    {
-        name: "USDCRealT",
-        symbol: "USDCRealT",
-        contractAddress: "0x3e7493506Bc350Ed7f5344196B1842185753bde1"
-    },
-    {
-        name: "WETHRealT",
-        symbol: "WETHRealT",
-        contractAddress: "0x292C5840EfE7C3282Ad2EB88a53cDBF2841F0917"
-    },
-    {
-        name: "WXDAIRealT",
-        symbol: "WXDAIRealT",
-        contractAddress: "0x803029DB36f37D130d8A005A62c55D17383f6f15"
-    }
-]
+  {
+    name: 'USDCRealT',
+    symbol: 'USDCRealT',
+    contractAddress: '0x3e7493506Bc350Ed7f5344196B1842185753bde1',
+  },
+  {
+    name: 'WETHRealT',
+    symbol: 'WETHRealT',
+    contractAddress: '0x292C5840EfE7C3282Ad2EB88a53cDBF2841F0917',
+  },
+  {
+    name: 'WXDAIRealT',
+    symbol: 'WXDAIRealT',
+    contractAddress: '0x803029DB36f37D130d8A005A62c55D17383f6f15',
+  },
+];
+export const ethereumAllowedBuyTokens: AllowedBuyToken[] = [
+  {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  },
+  {
+    name: 'Dai',
+    symbol: 'DAI',
+    contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  },
+  {
+    name: 'Tether USDT',
+    symbol: 'USDT',
+    contractAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  },
+  {
+    name: 'Liquity USD',
+    symbol: 'LUSD',
+    contractAddress: '0x5f98805a4e8be255a32880fdec7f6728c6568ba0',
+  },
+];
+export const gnosisAllowedBuyTokens: AllowedBuyToken[] = [
+  {
+    name: 'USD Coin',
+    symbol: 'USDC',
+    contractAddress: '0xddafbb505ad214d7b80b1f830fccc89b60fb7a83',
+  },
+  {
+    name: 'Wrapped XDAI',
+    symbol: 'WXDAI',
+    contractAddress: '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d',
+  },
+  {
+    name: 'RMM Xdai',
+    symbol: 'armmWXDAI',
+    contractAddress: '0x7349C9eaA538e118725a6130e0f8341509b9f8A0',
+  },
+  {
+    name: 'Mai stablecoin',
+    symbol: 'MAI',
+    contractAddress: '0x3f56e0c36d275367b8c502090edf38289b3dea0d',
+  },
+];
--- src/hooks/useAllowedBuyTokens.ts
+++ src/hooks/useAllowedBuyTokens.ts
@@ -1,28 +1,37 @@
+import { useEffect, useState } from 'react';
 
-import { useWeb3React } from "@web3-react/core";
-import { useEffect, useState } from "react"
-import { goerliAllowedBuyTokens } from "src/constants/allowedBuyTokens";
-import { AllowedBuyToken } from "src/types/allowedBuyTokens";
+import { useWeb3React } from '@web3-react/core';
+
+import {
+  ethereumAllowedBuyTokens,
+  gnosisAllowedBuyTokens,
+  goerliAllowedBuyTokens,
+} from 'src/constants/allowedBuyTokens';
+import { AllowedBuyToken } from 'src/types/allowedBuyTokens';
 
 type useAllowedBuyTokensReturn = {
-    allowedBuyTokens: AllowedBuyToken[]
-}
+  allowedBuyTokens: AllowedBuyToken[];
+};
 
-const getRightAllowBuyTokens = (chainId: number|undefined): AllowedBuyToken[]  => {
-    switch(chainId){
-        case 5:
-            return goerliAllowedBuyTokens;
-        default: 
-            return [];
-    }
-}
+const getRightAllowBuyTokens = (
+  chainId: number | undefined
+): AllowedBuyToken[] => {
+  switch (chainId) {
+    case 1:
+      return ethereumAllowedBuyTokens;
+    case 5:
+      return goerliAllowedBuyTokens;
+    case 100:
+      return gnosisAllowedBuyTokens;
+    default:
+      return [];
+  }
+};
 
 export const useAllowedBuyTokens = (): useAllowedBuyTokensReturn => {
+  const { chainId } = useWeb3React();
 
-    const { chainId } = useWeb3React();
-
-    return{
-        allowedBuyTokens: getRightAllowBuyTokens(chainId)
-    }
-
-}
+  return {
+    allowedBuyTokens: getRightAllowBuyTokens(chainId),
+  };
+};
