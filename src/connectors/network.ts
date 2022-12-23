5c5089214559d1e30a90cc7ff46eea0ed1f376a4
Michael-RealT <Michael@realt.co>
2022-12-24 00:42:27 +0100

    fix default network to Gnosis

--- src/connectors/network.ts
+++ src/connectors/network.ts
@@ -8,6 +8,6 @@ export const [network, hooks] = initializeConnector<Network>(
     new Network({
       actions: actions,
       urlMap: URLS,
-      defaultChainId: ChainsID.Goerli,
+      defaultChainId: ChainsID.Gnosis,
     })
 );
