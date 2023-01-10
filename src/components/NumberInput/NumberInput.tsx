ca05c1a2567bbe4425f67b282fa1d76e94f7e05f
Jean-Benoît <jeanbenoit@realt.co>
2023-01-10 11:00:54 +0100

    fix: max button

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -54,7 +54,7 @@ export const NumberInput: FRC<NumberInputProps, HTMLInputElement> = forwardRef(
             onClick={() => setFieldValue("amount",Number(props.max))}
             {...controlsProps}
           >
-            {!props.max ? <Loader size={"xs"}/> : 'Max'}
+            {props.max == undefined ? <Loader size={"xs"}/> : 'Max'}
           </Button>
         )}
       </Flex>
