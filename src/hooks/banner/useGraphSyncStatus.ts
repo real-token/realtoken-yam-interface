6ede51bcda601013a5b2d63f35f687b1fc8aa4ac
Jean-Benoît <jeanbenoit@realt.co>
2023-01-12 09:40:17 +0100

    fix: useGraphSyncStatus

--- src/hooks/banner/useGraphSyncStatus.ts
+++ src/hooks/banner/useGraphSyncStatus.ts
@@ -13,76 +13,79 @@ interface UseGraphSyncStatus {
   errorMessage: string;
 }
 
+const getBlockNumberFromTheGraph = (chainId: number) => new Promise<number>(async (resolve, reject) => {
+  try {
+    const client = new ApolloClient({
+      uri: getTheGraphUrlYAM(chainId ?? 100),
+      cache: new InMemoryCache(),
+    });
+
+    const { data } = await client.query({
+      query: gql`
+        query getStatus {
+          _meta {
+            block {
+              hash
+              number
+              timestamp
+            }
+          }
+        }
+      `,
+    });
+
+    const blockNumber = data._meta.block.number;
+    resolve(blockNumber);
+  } catch (err) {
+    console.log(err);
+    reject(err);
+  }
+});
+
+const getBlockNumberFromRpc = (chainId: number) => new Promise<number>(async (resolve, reject) => {
+try {
+  if (chainId) {
+    const { rpcUrl } = CHAINS[chainId as ChainsID];
+    console.log('rpcUrl', rpcUrl);
+    await fetch(rpcUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'eth_getBlockByNumber',
+        params: ['latest', false],
+        id: 1,
+      }),
+    }).then((response) =>
+      response.json().then((data) => {
+        resolve(new BigNumber(data.result.number).toNumber());
+      })
+    );
+  }
+} catch (err) {
+  console.log(err);
+  reject(err);
+}
+});
+
 export const useGraphSyncStatus = (): UseGraphSyncStatus => {
+
+  const [init,setInit] = useState<boolean>(false);
   const [isOk, setIsOk] = useState<boolean>(false);
   const { provider, chainId } = useWeb3React();
 
-  const getBlockNumberFromTheGraph = new Promise<number>(
-    async (resolve, reject) => {
-      try {
-        const client = new ApolloClient({
-          uri: getTheGraphUrlYAM(chainId ?? 100),
-          cache: new InMemoryCache(),
-        });
-
-        const { data } = await client.query({
-          query: gql`
-            query getStatus {
-              _meta {
-                block {
-                  hash
-                  number
-                  timestamp
-                }
-              }
-            }
-          `,
-        });
-
-        const blockNumber = data._meta.block.number;
-        resolve(blockNumber);
-      } catch (err) {
-        console.log(err);
-        reject(err);
-      }
-    }
-  );
-
-  const getBlockNumberFromRpc = new Promise<number>(async (resolve, reject) => {
-    try {
-      if (chainId) {
-        const { rpcUrl } = CHAINS[chainId as ChainsID];
-        console.log('rpcUrl', rpcUrl);
-        const response = await fetch(rpcUrl, {
-          method: 'POST',
-          body: JSON.stringify({
-            jsonrpc: '2.0',
-            method: 'eth_getBlockByNumber',
-            params: ['latest', false],
-            id: 1,
-          }),
-        }).then((response) =>
-          response.json().then((data) => {
-            resolve(new BigNumber(data.result.number).toNumber());
-          })
-        );
-      }
-    } catch (err) {
-      console.log(err);
-      reject(err);
-    }
-  });
-
   const getSync = async () => {
-    Promise.all([getBlockNumberFromTheGraph, getBlockNumberFromRpc]).then(
+    if(!chainId) return;
+    Promise.all([getBlockNumberFromTheGraph(chainId), getBlockNumberFromRpc(chainId)]).then(
       (values) => {
+        // console.log("GET SYNC")
         if (values[0] - values[1] < 10) setIsOk(true);
+        setInit(true);
       }
     );
   };
 
   useEffect(() => {
-    if (provider && chainId) getSync();
+    if (provider && chainId && !init) getSync();
   }, [provider, chainId]);
 
   return {
