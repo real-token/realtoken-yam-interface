32c2b26f2029616d2ed2a4ac294a0d6e71a62e3f
Jean-Benoît <jeanbenoit@realt.co>
2023-01-17 11:53:50 +0100

    feat: add info when no wallet connected

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,8 @@
-import { createStyles } from '@mantine/core';
+import { createStyles, Flex, Text } from '@mantine/core';
+import { IconWalletOff } from '@tabler/icons';
+import { useWeb3React } from '@web3-react/core';
 import { FC, ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 
@@ -23,12 +26,23 @@ const useStyles = createStyles((theme) => ({
 export const Layout: FC<LayoutProps> = ({ children }) => {
 
   const { classes } = useStyles();
+  const { account } = useWeb3React();
+
+  const { t } = useTranslation("common", { keyPrefix: "general" });
 
   return (
     <div className={classes.container}>
       <Header />
       <div className={classes.main}>
-        {children}
+        {/* {children} */}
+        { account ? 
+            children 
+          : 
+            <Flex style={{ width: "100%", height: "100%" }} justify={"center"} align={"center"} gap={"sm"}>
+                <IconWalletOff size={36}/>
+                <Text fw={700} fz={"xl"}>{t("noConnectedWallet")}</Text>
+            </Flex>
+        }
       </div>
       <Footer />
     </div>
