328f8882b161a63b60ad7b94182fba4c91feddd1
Jean-Benoît <jeanbenoit@realt.co>
2023-01-27 19:28:41 +0100

    feat: add delta for OfferYield

--- src/components/Column/OfferYield.tsx
+++ src/components/Column/OfferYield.tsx
@@ -1,4 +1,4 @@
-import { Flex, Skeleton } from "@mantine/core"
+import { Flex, Skeleton, Text } from "@mantine/core"
 import BigNumber from "bignumber.js";
 import { useMemo } from "react";
 import { useOraclePriceFeed } from "src/hooks/useOraclePriceFeed";
@@ -23,11 +23,26 @@ export const OfferYield = ({ offer } : OfferYieldProps) => {
         if(!propertyToken || !propertyToken.netRentYearPerToken || !tokenPriceInDollar) return undefined;
         const offerAdjusted = new BigNumber(propertyToken.netRentYearPerToken).dividedBy(tokenPriceInDollar);
         return offerAdjusted;
-    },[propertyToken, tokenPriceInDollar])
+    },[propertyToken, tokenPriceInDollar]);
+
+    const offerDelta: BigNumber|undefined = useMemo(() => {
+        if(!offerAjustedYield || !propertyToken) return undefined;
+
+        const originalYield = propertyToken.annualYield;
+        return originalYield ? offerAjustedYield.multipliedBy(new BigNumber(1)).dividedBy(new BigNumber(originalYield)).minus(1) : undefined;
+
+    },[offerAjustedYield, propertyToken])
 
     return(
-        <Flex>
-           {offerAjustedYield ? `${parseFloat(offerAjustedYield.multipliedBy(100).toString()).toFixed(2)}%` : <Skeleton height={15}/>}
+        <Flex gap={"sm"}>
+            { offerAjustedYield && offerDelta ?
+            <>
+            <Text>{`${parseFloat(offerAjustedYield.multipliedBy(100).toString()).toFixed(2)}%`}</Text>
+            <Text color={offerDelta.gt(0) ? "green" : "red"}>{`(${offerDelta.gt(0) ? "+" : ""}${parseFloat(offerDelta.multipliedBy(100).toString()).toFixed(2)}%)`}</Text>
+            </>
+           :
+           <Skeleton height={15}/>
+            }
         </Flex>
     )
 }
