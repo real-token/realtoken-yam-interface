e1872422c3f4d19cf47e93764e228ccfd4ed4f7d
Jean-Benoît <jeanbenoit@realt.co>
2023-02-04 16:07:47 +0100

    fix: build error

--- src/hooks/offers/useOffer.ts
+++ src/hooks/offers/useOffer.ts
@@ -1,3 +1,4 @@
+import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
 import { useCallback, useEffect, useState } from "react"
 import { fetchOffer } from "src/utils/offers/fetchOffer";
@@ -10,14 +11,14 @@ type UseOfferProps  = (offerId: number) => {
 };
 
 export const useOffer: UseOfferProps = (offerId: number) => {
-    const { chainId } = useWeb3React();
+    const { chainId, provider } = useWeb3React();
     const [offer,setOffer] = useState<Offer>(DEFAULT_OFFER);
     const [isLoading,setIsLoading] = useState<boolean>(true);
 
     const { propertiesToken, propertiesIsloading } = usePropertiesToken();
 
-    const fetch = useCallback(async (chainId: number, offerId: number) => {
-        fetchOffer(chainId,offerId, propertiesToken)
+    const fetch = useCallback(async (provider: Web3Provider, chainId: number, offerId: number) => {
+        fetchOffer(provider, chainId,offerId, propertiesToken)
             .then((offer: Offer) => { 
                 setOffer(offer);
                 setIsLoading(false);
@@ -26,8 +27,8 @@ export const useOffer: UseOfferProps = (offerId: number) => {
     },[propertiesToken])
 
     useEffect(() => {
-        if(offerId && chainId && propertiesToken && !propertiesIsloading && propertiesToken.length > 0) fetch(chainId, offerId);
-    },[offerId, chainId, propertiesIsloading, propertiesToken, fetch])
+        if(offerId && chainId && provider && propertiesToken && !propertiesIsloading && propertiesToken.length > 0) fetch(provider,chainId, offerId);
+    },[offerId, chainId, propertiesIsloading,provider, propertiesToken, fetch])
 
     return{
         offer,
--- src/utils/offers/fetchOffer.ts
+++ src/utils/offers/fetchOffer.ts
@@ -7,8 +7,9 @@ import { Offer as OfferGraphQl } from '../../../.graphclient/index';
 import { getRealTokenClient, getYamClient } from "./getClientURL";
 import { gql } from "@apollo/client";
 import { getOfferQuery } from "./getOfferQuery";
+import { Web3Provider } from "@ethersproject/providers";
 
-export const fetchOffer = (chainId: number, offerId: number, propertiesToken: PropertiesToken[]): Promise<Offer> => {
+export const fetchOffer = (provider: Web3Provider, chainId: number, offerId: number, propertiesToken: PropertiesToken[]): Promise<Offer> => {
     return new Promise(async (resolve,reject) => {
       try{
 
@@ -32,7 +33,7 @@ export const fetchOffer = (chainId: number, offerId: number, propertiesToken: Pr
         const realtokenData: [DataRealtokenType] = await getBigDataGraphRealtoken(chainId, clientRealToken, batch);
 
         const accountUser = realtokenData[0];
-        const offer = await parseOffer(offerFromTheGraph,accountUser);
+        const offer = await parseOffer(provider,offerFromTheGraph,accountUser,propertiesToken);
   
         const hasPropertyToken = propertiesToken.find(propertyToken => (propertyToken.contractAddress == offer.buyerTokenAddress || propertyToken.contractAddress == offer.offerTokenAddress));
         offer.hasPropertyToken = hasPropertyToken ? true : false;
