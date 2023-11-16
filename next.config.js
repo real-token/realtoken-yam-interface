994ad8bc10ddea87e13fba04411f5b18f670a3f1
jeanbenoit-richez <jeanbenoit@realt.co>
2023-11-16 12:45:04 +0200

    fix: local gnosis-safe

--- next.config.js
+++ next.config.js
@@ -6,6 +6,28 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
+  async headers() {
+    return [
+        {
+          //
+            source: "/manifest.json",
+            headers: [
+              {
+                key: "Access-Control-Allow-Origin",
+                value: 'https://app.safe.global',
+              },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: 'GET',
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value: 'X-Requested-With, content-type, Authorization',
+              },
+            ]
+        }
+    ]
+}
 };
 
 module.exports = nextConfig;
