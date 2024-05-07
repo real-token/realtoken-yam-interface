d69e3c0985f5b7bf5c6f47de74e0fe60bb183a30
Cyrille <cdurbal@cleansatmining.com>
2024-05-07 11:03:38 +0200

    feat: add help menu

--- src/components/HeaderNav/HeaderNav.tsx
+++ src/components/HeaderNav/HeaderNav.tsx
@@ -9,44 +9,53 @@ import { isRole, USER_ROLE } from 'src/types/admin';
 
 export const HeaderNav: FC = () => {
   const { t } = useTranslation('header');
-  const router = useRouter()
+  const router = useRouter();
   const colorSelected = '#606e0c';
 
   const { role } = useRole();
 
   return (
-      <Flex sx={styles.container} gap={100}>
-        <Text
-          size={'xl'}
-          weight={700}
-          component={NextLink}
-          href={'/'}
-          color={router.pathname === '/' ? colorSelected : ''}
-        >
-          {t('titleCat1')}
-        </Text>
-        <Text
-          size={'xl'}
-          weight={700}
-          component={NextLink}
-          href={'/my-offers'}
-          color={router.pathname === '/my-offers' ? colorSelected : ''}
-        >
-          {t('titleCat2')}
-        </Text>
-        { isRole(role,[USER_ROLE.MODERATOR,USER_ROLE.ADMIN]) ? 
-            <Text
-            size={'xl'}
-            weight={700}
-            component={NextLink}
-            href={'/admin'}
-            color={router.pathname === '/admin' ? colorSelected : ''}
-          >
-            {t('titleAdmin')}
-          </Text>
-            :
-            undefined
+    <Flex sx={styles.container} gap={100}>
+      <Text
+        size={'xl'}
+        weight={700}
+        component={NextLink}
+        href={'/'}
+        color={router.pathname === '/' ? colorSelected : ''}
+      >
+        {t('titleCat1')}
+      </Text>
+      <Text
+        size={'xl'}
+        weight={700}
+        component={NextLink}
+        href={'/my-offers'}
+        color={router.pathname === '/my-offers' ? colorSelected : ''}
+      >
+        {t('titleCat2')}
+      </Text>
+      <Text
+        size={'xl'}
+        weight={700}
+        component={NextLink}
+        href={
+          'https://cleansat-mining.gitbook.io/gitbook-cleansat-mining#le-marche-secondaire-yam'
         }
+        target={'_blank'}
+      >
+        {t('titleHelp')}
+      </Text>
+      {isRole(role, [USER_ROLE.MODERATOR, USER_ROLE.ADMIN]) ? (
+        <Text
+          size={'xl'}
+          weight={700}
+          component={NextLink}
+          href={'/admin'}
+          color={router.pathname === '/admin' ? colorSelected : ''}
+        >
+          {t('titleAdmin')}
+        </Text>
+      ) : undefined}
     </Flex>
   );
 };
