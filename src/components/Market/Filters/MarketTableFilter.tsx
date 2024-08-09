85ea656fde4f9c2ec23b1a17211e6cb9c7a9a2b7
Nandy Bâ <nandyba@yahoo.fr>
2024-08-09 22:30:13 +0200

    add hide dust checkbox

--- src/components/Market/Filters/MarketTableFilter.tsx
+++ src/components/Market/Filters/MarketTableFilter.tsx
@@ -1,32 +1,34 @@
-import { Flex, Text, TextInput } from "@mantine/core";
-import { FC } from "react";
-import { useTranslation } from "react-i18next";
-import { ShowOnlyWlCheckbox } from "./OnlyShowWLCheckbox";
-import { useAtom } from "jotai";
-import { nameFilterValueAtom } from "../../../states";
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { Flex, Text, TextInput } from '@mantine/core';
+
+import { useAtom } from 'jotai';
+
+import { nameFilterValueAtom } from '../../../states';
+import { HideDustCheckbox } from './HideDustCheckbox';
+import { ShowOnlyWlCheckbox } from './OnlyShowWLCheckbox';
 
 export const MarketTableFilter: FC = () => {
+  const { t } = useTranslation('table', { keyPrefix: 'filters' });
+  const [nameFilterValue, setNamefilterValue] = useAtom(nameFilterValueAtom);
 
-    const { t } = useTranslation('table', { keyPrefix: 'filters' });
-    const [nameFilterValue,setNamefilterValue] = useAtom(nameFilterValueAtom);
-    
-    return(
-        <Flex
-            gap={"xs"}
-            align={"flex-start"}
-            direction={"column"}
-            wrap={"wrap"}
-            mb={20}
-        >
-            <Text size={'xl'}>
-                {t('title')}
-            </Text>
-            <TextInput 
-                placeholder={t('nameFilterPlaceholder')}
-                value={nameFilterValue}
-                onChange={(event) => setNamefilterValue(event.currentTarget.value)}
-            />
-            <ShowOnlyWlCheckbox />
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      gap={'xs'}
+      align={'flex-start'}
+      direction={'column'}
+      wrap={'wrap'}
+      mb={20}
+    >
+      <Text size={'xl'}>{t('title')}</Text>
+      <TextInput
+        placeholder={t('nameFilterPlaceholder')}
+        value={nameFilterValue}
+        onChange={(event) => setNamefilterValue(event.currentTarget.value)}
+      />
+      <ShowOnlyWlCheckbox />
+      <HideDustCheckbox />
+    </Flex>
+  );
+};
