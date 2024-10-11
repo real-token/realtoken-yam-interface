c92714f42914a1bf265d44260b79886c482d806c
jeanbenoit-richez <jeanbenoit@realt.co>
2024-10-11 17:38:13 +0300

    fix: buy button

--- src/components/Market/BuyActions/BuyActionsWithPermit.tsx
+++ src/components/Market/BuyActions/BuyActionsWithPermit.tsx
@@ -1,7 +1,7 @@
 import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { ActionIcon, Group, Title, Popover, Text, Flex, Button } from '@mantine/core';
+import { Group, Popover, Text, Flex, Button, ActionIcon, Tooltip } from '@mantine/core';
 import { useModals } from '@mantine/modals';
 import { IconShoppingCart } from '@tabler/icons';
 import { useWeb3React } from '@web3-react/core';
@@ -11,8 +11,6 @@ import { useRefreshOffers } from 'src/hooks/offers/useRefreshOffers';
 import { PropertiesToken } from '../../../types';
 import { getNotWhitelistedTokens } from '../../../utils/whitelist';
 import { useRootStore } from '../../../zustandStore/store';
-import { UpdateActionsWithPermit } from '../UpdateActions';
-import { DeleteActions } from '../DeleteActions';
 
 type BuyActions = {
   buyOffer: Offer | undefined;
@@ -68,24 +66,28 @@ export const BuyActionsWithPermit: FC<BuyActions> = ({
     });
   }, [modals, t]);
 
-  const isAccountOffer: boolean = useMemo(() => {
-    if(!buyOffer || !account) return false;
-    return buyOffer.sellerAddress == account.toLowerCase()
-  },[buyOffer, account]);
-
-  const [opened, setOpened] = useState(false);
   const [tokenNotWhitelisted, setTokenNotWhitelisted] = useState<PropertiesToken[]>([]);
   useEffect(() => {
     if(!wlProperties || !buyOffer) return;
-
     const notWlTokens = getNotWhitelistedTokens(wlProperties, buyOffer, properties);
     setTokenNotWhitelisted(notWlTokens);
-
   }, [wlProperties, buyOffer, properties]);
 
+  const isAccountOffer = useMemo(() => {
+    if(!buyOffer || !account) return false;
+    return buyOffer.sellerAddress == account.toLowerCase()
+  },[buyOffer, account])
+
   const cannotBuy = useMemo(() => {
+    if(!buyOffer || !account) return false;
     return isAccountOffer || buyOffer == undefined
-  },[isAccountOffer, buyOffer])
+  },[buyOffer, account])
+
+  const [opened, setOpened] = useState(false);
+  const hovered = useCallback((state: boolean) => {
+    if(tokenNotWhitelisted.length > 0) setOpened(true)
+    if(isAccountOffer) setOpened(true)
+  },[tokenNotWhitelisted, isAccountOffer])
 
   return (
     <>
@@ -102,32 +104,36 @@ export const BuyActionsWithPermit: FC<BuyActions> = ({
             <Group 
               justify={'center'}
               className={groupClassName ?? ""}
-              onMouseEnter={() => { if(tokenNotWhitelisted.length > 0) setOpened(true) }}
-              onMouseLeave={() => { setOpened(false) }}
+              onMouseEnter={() => hovered(true)}
+              onMouseLeave={() => hovered(false)}
             >
-              <Button
+              <ActionIcon
                 color={'green'}
                 onClick={() =>
                   account && buyOffer ? onOpenBuyModal(buyOffer) : onOpenWalletModal()
                 }
                 className={buttonClassName ?? ""}
                 disabled={tokenNotWhitelisted.length > 0 || !buyOffer || cannotBuy}
-                leftSection={<IconShoppingCart size={16} aria-label={'Buy'} />}
                 style={{ width: '100%' }}
               >
-                {!isAccountOffer && buyOffer ? t('') : "Cannot buy your own offer"}
-              </Button>
+                <IconShoppingCart size={16} aria-label={'Buy'} />
+              </ActionIcon>
+              {/* "Cannot buy your own offer" */}
             </Group> 
           </Popover.Target>
           <Popover.Dropdown>
-            <Flex>
-              <Text>{t1('notWhitelisted')}</Text>
-            </Flex>
-            <ul>
-              {tokenNotWhitelisted.length > 0 && tokenNotWhitelisted.map((token, index) => (
-                <li key={`not-wl-${index}`}><Text>{token.shortName}</Text></li>
-              ))}
-            </ul>
+            {tokenNotWhitelisted.length > 0 ? (
+              <>
+                <Text>{t1('notWhitelisted')}</Text>
+                <ul>
+                  {tokenNotWhitelisted.length > 0 && tokenNotWhitelisted.map((token, index) => (
+                    <li key={`not-wl-${index}`}><Text>{token.shortName}</Text></li>
+                  ))}
+                </ul>
+              </>
+            ) : isAccountOffer ? (
+              <Text>{t1('cannotBuyYourOffer')}</Text>
+            ) : undefined}
           </Popover.Dropdown>
         </Popover>
       ): undefined }
