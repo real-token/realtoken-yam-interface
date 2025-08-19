0644794adc49986119972e6e48c9578a3a7a6416
Michael-RealT <Michael@realt.co>
2025-08-19 22:20:16 +0200

    feat: ajout de la vérification de la liste blanche universelle et des tokens sans vérification WL

--- src/utils/whitelist.ts
+++ src/utils/whitelist.ts
@@ -1,39 +1,48 @@
-import { WHITLISTED_ON_ALL_ID_RULE } from "../constants/whitelist";
-import { PropertiesToken } from "../types";
-import { OFFER_TYPE, Offer } from "../types/offer";
+import {
+  TOKEN_ID_NO_WL_NEEDED,
+  isUniversalWhitelisted,
+} from '../constants/whitelist';
+import { PropertiesToken } from '../types';
+import { OFFER_TYPE, Offer } from '../types/offer';
 
 export const getNotWhitelistedTokens = (
-    wlTokenId: number[],
-    offer: Offer,
-    properties: PropertiesToken[]
+  wlTokenId: number[],
+  offer: Offer,
+  properties: PropertiesToken[]
 ): PropertiesToken[] => {
+  // Bypass WL universel
+  if (isUniversalWhitelisted(wlTokenId)) return [];
 
-    // Bypass 
-    if(wlTokenId.some(r=> WHITLISTED_ON_ALL_ID_RULE.includes(r))) return [];
+  let tokenAddressToCheck: string[] = [];
+  switch (offer.type) {
+    case OFFER_TYPE.BUY:
+      tokenAddressToCheck = [offer.buyerTokenAddress];
+      break;
+    case OFFER_TYPE.SELL:
+      tokenAddressToCheck = [offer.offerTokenAddress];
+      break;
+    case OFFER_TYPE.EXCHANGE:
+      tokenAddressToCheck = [offer.buyerTokenAddress, offer.offerTokenAddress];
+      break;
+  }
 
-    let tokenAddressToCheck: string[] = [];
-    switch(offer.type){
-        case OFFER_TYPE.BUY:
-            tokenAddressToCheck = [offer.buyerTokenAddress];
-        break;
-        case OFFER_TYPE.SELL:
-            tokenAddressToCheck = [offer.offerTokenAddress];
-        break;
-        case OFFER_TYPE.EXCHANGE:
-            tokenAddressToCheck = [offer.buyerTokenAddress, offer.offerTokenAddress];
-        break;
+  const tokenNoWlNeeded = TOKEN_ID_NO_WL_NEEDED;
+
+  const notWlTokens: PropertiesToken[] = [];
+  tokenAddressToCheck.forEach((tokenAddress: string) => {
+    const token = properties.find(
+      (token: PropertiesToken) =>
+        token.contractAddress.toLowerCase() == tokenAddress.toLowerCase()
+    );
+
+    if (
+      token &&
+      !wlTokenId.includes(token.tokenIdRules) &&
+      !tokenNoWlNeeded.includes(token?.tokenIdRules)
+    ) {
+      notWlTokens.push(token);
     }
+  });
 
-    const tokenNoWlNeeded = [100508];
-
-    const notWlTokens: PropertiesToken[] = [];
-    tokenAddressToCheck.forEach((tokenAddress: string) => {
-        const token = properties.find((token: PropertiesToken) => token.contractAddress.toLowerCase() == tokenAddress.toLowerCase());
-        
-        if (token && !wlTokenId.includes(token.tokenIdRules) && !tokenNoWlNeeded.includes(token?.tokenIdRules)) {
-            notWlTokens.push(token);
-        }
-    });
-
-    return notWlTokens;
-}
+  return notWlTokens;
+};
