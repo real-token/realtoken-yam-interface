d7458212f5e24bfc4c41ebf66b300a7860f8e955
jeanbenoit-richez <jeanbenoit@realt.co>
2025-12-09 09:00:26 +0200

    fix: debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import { NextPage } from 'next';
+
+import { Flex } from '@mantine/core';
+import { useWeb3AuthPrivate } from '@real-token/web3';
+
+import Display from 'src/components/Display/Display';
 import 'src/components/Market';
 import { MarketTableFilter } from 'src/components/Market/Filters';
-import { Flex } from '@mantine/core';
-import Display from 'src/components/Display/Display';
 import { ConnectedProvider } from 'src/providers/ConnectProvider';
 
 const HomePage: NextPage = () => {
+  const web3Auth = useWeb3AuthPrivate();
+  console.dir(web3Auth, { depth: 10 });
+
   return (
     <ConnectedProvider>
-      <Flex my={"xl"} direction={"column"}>
+      <Flex my={'xl'} direction={'column'}>
         <MarketTableFilter />
         <Display />
       </Flex>
